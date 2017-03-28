1487c8c68d157876601e88f862e02e003927c0d4
dehein <dehein@users.noreply.github.com>
2017-03-28 20:33:04 +0200

    updated

--- src/config.h
+++ src/config.h
@@ -2,20 +2,16 @@
 /* Replace private config information marked with XXXXXXXXXXXXXXXXX in here with your own.  */
 /*** IFTTT Config ***/
 /* IFTTT Key */
-const char *key = "XXXXXXXXXXXXXXXXX";
+const char key[] = "XXXXXXXXXXXXXXXXX";
+
+/* The SSID and password of your Wi-Fi access point */
+const char ssid[] = "XXXXXXXXXXXXXXXXX";
+const char password[] = "XXXXXXXXXXXXXXXXX";
 
 /* IFTTT SSL Fingerprint */
-const char* fingerprint = "A9 81 E1 35 B3 7F 81 B9 87 9D 11 DD 48 55 43 2C 8F C3 EC 87";
+const char* fingerprint = "A9:81:E1:35:B3:7F:81:B9:87:9D:11:DD:48:55:43:2C:8F:C3:EC:87";
 
 /* Host config */
 const char* host = "maker.ifttt.com";
 const char* event = "briefkasten";
 const int httpsPort = 443;
-
-/*** WiFi Config ***/
-const char* ssid = "XXXXXXXXXXXXXXXXX";
-const char* password = "XXXXXXXXXXXXXXXXX";
-/* Replace IP with your own Information */
-const IPAddress ip(192, 168, 178, 40);
-const IPAddress gateway(192, 168, 178, 1);
-const IPAddress subnet(255, 255, 255, 0);
